c1e5130fc055e9aad3ebaa84b7b89b4f5a7eddae
iryna <irakirvas78@gmail.com>
2025-09-21 17:17:43 +0300

    feat(index): implement index page with view, template and URL

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from .models import Dish, DishType
 
-# Create your views here.
+
+def index(request):
+    num_cooks = get_user_model().objects.filter(is_staff=False).count()
+    num_dishes = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+    context = {"num_cooks": num_cooks, "num_dishes": num_dishes,
+               "num_dish_types": num_dish_types}
+    return render(request, "kitchen/index.html", context=context)

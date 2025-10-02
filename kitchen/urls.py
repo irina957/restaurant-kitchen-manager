3556fdfdfc312dcc578e42f8443742f9810830df
iryna <irakirvas78@gmail.com>
2025-10-02 18:56:40 +0300

    feat: add URL patterns and corresponding views for DishType, Dish, and Cook lists

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from kitchen.views import index
+from kitchen.views import index, DishTypeListView, DishListView, CookListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
+    path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("cooks/", CookListView.as_view(), name="cook-list"),
     ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
-from .models import Dish, DishType
+from django.views import generic
+
+from .models import Dish, DishType, Cook
 
 
 def index(request):
@@ -10,3 +12,17 @@ def index(request):
     context = {"num_cooks": num_cooks, "num_dishes": num_dishes,
                "num_dish_types": num_dish_types}
     return render(request, "kitchen/index.html", context=context)
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    template_name = "kitchen/dish_type_list.html"
+    context_object_name = "dish_type_list"
+
+
+class DishListView(generic.ListView):
+    model = Dish
+
+
+class CookListView(generic.ListView):
+    model = Cook

e350d603a0470d20bf1d56cb20457d5adfc93798
iryna <irakirvas78@gmail.com>
2025-10-05 20:25:59 +0300

    feat: add home page visits counter

--- kitchen/views.py
+++ kitchen/views.py
@@ -9,8 +9,11 @@ def index(request):
     num_cooks = get_user_model().objects.filter(is_staff=False).count()
     num_dishes = Dish.objects.count()
     num_dish_types = DishType.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     context = {"num_cooks": num_cooks, "num_dishes": num_dishes,
-               "num_dish_types": num_dish_types}
+               "num_dish_types": num_dish_types,
+               "num_visits": num_visits}
     return render(request, "kitchen/index.html", context=context)
 
 

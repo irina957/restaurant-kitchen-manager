5d4ab83e0fdd278a1f6da7f4dfb38c25f42d4eb9
iryna <irakirvas78@gmail.com>
2025-10-08 15:24:51 +0300

    feat: integrate django-crispy-forms v4 into project settings and forms

--- restaurant_manager/settings.py
+++ restaurant_manager/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "kitchen",
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "kitchen.Cook"
 
 LOGIN_REDIRECT_URL = "/"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"

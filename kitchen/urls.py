d253dd4337633c1d5e0cc4353b86f9ad22940f89
iryna <irakirvas78@gmail.com>
2025-10-09 14:59:34 +0300

    feat: add Update and Delete functionality for DishType

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,17 +1,22 @@
 from django.urls import path
 
 from kitchen.views import index, DishTypeListView, DishListView, CookListView, DishDetailView, CookDetailView, \
-    DishCreateView, DishTypeCreateView
+    DishCreateView, DishTypeCreateView, DishTypeUpdateView, DishTypeDeleteView
 
 urlpatterns = [
     path("", index, name="index"),
     path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
-    path("dishes/", DishListView.as_view(), name="dish-list"),
-    path("cooks/", CookListView.as_view(), name="cook-list"),
-    path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
-    path("cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
-    path("dishes/create/", DishCreateView.as_view(), name="dish-create"),
     path("dish-types/create/", DishTypeCreateView.as_view(), name="dish-type-create"),
+    path("dish-types/<int:pk>/update/", DishTypeUpdateView.as_view(), name="dish-type-update"),
+    path("dish-types/<int:pk>/delete/", DishTypeDeleteView.as_view(), name="dish-type-delete"),
+    path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+    path("dishes/create/", DishCreateView.as_view(), name="dish-create"),
+    path("cooks/", CookListView.as_view(), name="cook-list"),
+
+    path("cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
+
+
     ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -59,3 +59,16 @@ class DishTypeCreateView(LoginRequiredMixin, generic.CreateView):
     fields = "__all__"
     success_url = reverse_lazy("kitchen:dish-type-list")
     template_name = "kitchen/dish_type_form.html"
+
+
+class DishTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = DishType
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:dish-type-list")
+    template_name = "kitchen/dish_type_form.html"
+
+
+class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = DishType
+    template_name = "kitchen/dish_type_confirm_delete.html"
+    success_url = reverse_lazy("kitchen:dish-type-list")

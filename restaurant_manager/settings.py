64a92ea37c55715bca7dc8af35faf60a413cbfbf
iryna <irakirvas78@gmail.com>
2025-10-15 12:44:09 +0300

    feat: add ASSETS_ROOT setting and context processor

--- restaurant_manager/settings.py
+++ restaurant_manager/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -64,6 +63,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "kitchen.context_processors.cfg_assets_root",
             ],
         },
     },
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+ASSETS_ROOT = "static/assets"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
